f30699ab57174a46e4d1d8c6c3d36e945bdb2c56
sagiftsa <giftkabelo1989@gmail.com>
2021-06-11 12:25:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pnejud
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pnejud)
---
+++ src/app/button-harness-example.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Testing with MatButtonHarness
+ */
+@Component({
+  selector: 'button-harness-example',
+  templateUrl: 'button-harness-example.html',
+})
+export class ButtonHarnessExample  {
+  clicked = false;
+}
+
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
